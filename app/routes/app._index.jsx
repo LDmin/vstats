5ad944292b275f6a91ae6286735320513a4c4885
dongmin.lu <dongmin.lu@davyin.com>
2024-03-05 14:15:36 +0800

    test

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -1,6 +1,11 @@
 import { useEffect } from "react";
 import { json } from "@remix-run/node";
-import { useActionData, useLoaderData, useNavigation, useSubmit } from "@remix-run/react";
+import {
+  useActionData,
+  useLoaderData,
+  useNavigation,
+  useSubmit,
+} from "@remix-run/react";
 import {
   Page,
   Layout,
@@ -9,7 +14,7 @@ import {
   Button,
   BlockStack,
   Box,
-  List,
+  ResourceList,
   Link,
   InlineStack,
 } from "@shopify/polaris";
@@ -18,7 +23,7 @@ import { authenticate } from "../shopify.server";
 export const loader = async ({ request }) => {
   const { admin, session } = await authenticate.admin(request);
 
-  const carrierServices =  await admin.rest.resources.CarrierService.all({
+  const carrierServices = await admin.rest.resources.CarrierService.all({
     session: session,
   });
 
@@ -69,118 +74,39 @@ export const action = async ({ request }) => {
 };
 
 export default function Index() {
-  const {carrierServices} = useLoaderData();
-  const nav = useNavigation();
-  const actionData = useActionData();
-  const submit = useSubmit();
-  const isLoading =
-    ["loading", "submitting"].includes(nav.state) && nav.formMethod === "POST";
-  const productId = actionData?.product?.id.replace(
-    "gid://shopify/Product/",
-    "",
-  );
-console.log(carrierServices);
-  useEffect(() => {
-    if (productId) {
-      shopify.toast.show("Product created");
-    }
-  }, [productId]);
-  const generateProduct = () => submit({}, { replace: true, method: "POST" });
-
+  const { carrierServices } = useLoaderData();
+  console.log(carrierServices);
+  const rows = [
+    ["Emerald Silk Gown", "$875.00", 124689, 140, "$122,500.00"],
+    ["Mauve Cashmere Scarf", "$230.00", 124533, 83, "$19,090.00"],
+    [
+      "Navy Merino Wool Blazer with khaki chinos and yellow belt",
+      "$445.00",
+      124518,
+      32,
+      "$14,240.00",
+    ],
+  ];
   return (
     <Page>
-      <ui-title-bar title="Remix app template">
-        <button variant="primary" onClick={generateProduct}>
-          Generate a product
-        </button>
-      </ui-title-bar>
-      <BlockStack gap="500">
-        <Layout>
-          <Layout.Section>
-            <Card>
-              <BlockStack gap="500">
-                <BlockStack gap="200">
-                  <Text as="h2" variant="headingMd">
-                    Congrats on creating a new Shopify app 🎉
-                  </Text>
-                  <Text variant="bodyMd" as="p">
-                    This embedded app template uses{" "}
-                    <Link
-                      url="https://shopify.dev/docs/apps/tools/app-bridge"
-                      target="_blank"
-                      removeUnderline
-                    >
-                      App Bridge
-                    </Link>{" "}
-                    interface examples like an{" "}
-                    <Link url="/app/additional" removeUnderline>
-                      additional page in the app nav
-                    </Link>
-                    , as well as an{" "}
-                    <Link
-                      url="https://shopify.dev/docs/api/admin-graphql"
-                      target="_blank"
-                      removeUnderline
-                    >
-                      Admin GraphQL
-                    </Link>{" "}
-                    mutation demo, to provide a starting point for app
-                    development.
-                  </Text>
-                </BlockStack>
-                <BlockStack gap="200">
-                  <Text as="h3" variant="headingMd">
-                    Get started with products
-                  </Text>
-                  <Text as="p" variant="bodyMd">
-                    Generate a product with GraphQL and get the JSON output for
-                    that product. Learn more about the{" "}
-                    <Link
-                      url="https://shopify.dev/docs/api/admin-graphql/latest/mutations/productCreate"
-                      target="_blank"
-                      removeUnderline
-                    >
-                      productCreate
-                    </Link>{" "}
-                    mutation in our API references.
-                  </Text>
-                </BlockStack>
-                <InlineStack gap="300">
-                  <Button loading={isLoading} onClick={generateProduct}>
-                    Generate a product
-                  </Button>
-                  {actionData?.product && (
-                    <Button
-                      url={`shopify:admin/products/${productId}`}
-                      target="_blank"
-                      variant="plain"
-                    >
-                      View product
-                    </Button>
-                  )}
-                </InlineStack>
-                {actionData?.product && (
-                  <Box
-                    padding="400"
-                    background="bg-surface-active"
-                    borderWidth="025"
-                    borderRadius="200"
-                    borderColor="border"
-                    overflowX="scroll"
-                  >
-                    <pre style={{ margin: 0 }}>
-                      <code>{JSON.stringify(actionData.product, null, 2)}</code>
-                    </pre>
-                  </Box>
-                )}
-              </BlockStack>
-            </Card>
-          </Layout.Section>
-          <Layout.Section variant="oneThird">
-            { carrierServices }
-          </Layout.Section>
-        </Layout>
-      </BlockStack>
+      <DataTable
+        columnContentTypes={[
+          "text",
+          "numeric",
+          "numeric",
+          "numeric",
+          "numeric",
+        ]}
+        headings={[
+          "Product",
+          "Price",
+          "SKU Number",
+          "Net quantity",
+          "Net sales",
+        ]}
+        rows={rows}
+        totals={["", "", "", 255, "$155,830.00"]}
+      />
     </Page>
   );
 }
